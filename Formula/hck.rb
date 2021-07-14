1d91866c5f6e1fc28a7841c947cd3f4007459406
Seth Stadick <sstadick@gmail.com>
2021-07-14 17:44:27 -0600

    v0.5.1

--- Formula/hck.rb
+++ Formula/hck.rb
@@ -1,24 +1,21 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hck < Formula
-    version "0.5.0"
-    desc "Faster and more featureful clone of cut"
-    homepage "https://github.com/sstadick/hck"
-    license any_of: ["MIT", "Unlicense"]
+  desc "Faster and more featureful clone of cut"
+  version "0.5.1"
+  homepage "https://github.com/sstadick/hck"
+  license any_of: ["MIT", "Unlicense"]
 
-    if OS.linux?
-        # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.0/hck-linux-amd64 | shasum -a 256
-        url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-linux-amd64"
-        sha256 "fbc7b646d0c8b732debed1aa9753fec83c3c8d68d493a2056082a3164a66717d"
-    elsif OS.mac?
-        # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.0/hck-macos-amd64 | shasum -a 256
-        url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-macos-amd64"
-        sha256 "d267e501301dc067a017ba8cae8b8b2a84f48023c32bc0aa5d059939f0257ce0"
-    end
+  if OS.linux?
+    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.1/hck-linux-amd64 | shasum -a 256
+    url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-linux-amd64"
+    sha256 "4b56955c78f4a74c6b99b4ae3cceaafe75c058c0cbd374cc97ebca43d34492f9"
+  elsif OS.mac?
+    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.1/hck-macos-amd64 | shasum -a 256
+    url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-macos-amd64"
+    sha256 "5d5f7866382250cd629d185562690b8ed190906bdb457f7abec3eec8a4d3ca47"
+  end
 
-    def install
-        `mv hck-* hck`
-        bin.install "hck"
-    end
+  def install
+    `mv hck-* hck`
+    bin.install "hck"
+  end
 end
