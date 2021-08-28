097114eb792e569ba8fbd60a99a2bbefd6666edf
Seth Stadick <sstadick@gmail.com>
2021-08-28 11:32:14 -0600

    v0.6.1

--- Formula/hck.rb
+++ Formula/hck.rb
@@ -2,16 +2,16 @@ class Hck < Formula
   desc "Faster and more featureful clone of cut"
   homepage "https://github.com/sstadick/hck"
   license any_of: ["MIT", "Unlicense"]
-  version "0.5.4"
+  version "0.6.0"
 
   if OS.linux?
-    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.4/hck-linux-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.6.1/hck-linux-amd64 | shasum -a 256
     url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-linux-amd64"
-    sha256 "ef49aaa4e4740ee2f1f1d63813413c1fe08074b083afd340c1555bf0811387f7"
+    sha256 "ad148df78bfd6d8a282914651e5e18a53c07207db4493be30be6cef1b8fe067d"
   elsif OS.mac?
-    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.5.4/hck-macos-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/hck/releases/download/v0.6.1/hck-macos-amd64 | shasum -a 256
     url "https://github.com/sstadick/hck/releases/download/v#{version}/hck-macos-amd64"
-    sha256 "869fde3775d5f249338b8d1ae87e8707a27333a98297562904cbf700c8db20cf"
+    sha256 "aeec42f4236676d2b78bd7746add676c7c4f5c816b648baf4a7ece20676ec5bc"
   end
 
   def install
